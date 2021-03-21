0d0efa74e35117371afa074ca4a0b1816602573a
Jorta18400 <jforta.garcia@gmail.com>
2021-03-21 12:33:22 +0100

    Primer intento, no funciona pero no se por que

---
+++ Ejercicio_c.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<math.h>
+/////////////////////////////
+//Este programa obtiene el autovalor dominante de una matriz dada
+//mediante el método de las potencias
+////////////////////////////
+
+int main(void)
+{
+    double a, suma, vv, Avv;     //a es el autovalor dominante, suma es un auxiliar donde se almacenan valores, vv es el producto de v*v y Avv es Av*v
+    double A[2][2];  //A es la matriz que vamos a diagonalizar
+    double v[2], Av[2];  //v es el autovector que obtendremos de forma iterativa, Av es el producto de A y v
+    int i, j, k; //contadores
+
+    A[0][0]=9;
+    A[0][1]=2;  //Defino los valores que tendrán los elementos de matriz
+    A[1][0]=2;
+    A[1][1]=6;
+
+    v[0]=1; //Valores iniciales de v
+    v[1]=1;
+
+    for(k=0; k<=5; k++) //k es el número de iteraciones que se reaizan para determinar v mediante A*v iteradamente
+    {
+        for (i = 0; i<2; i++) //se recorre cada fila de matriz A
+        {
+                suma = 0; 
+                for (j = 0; j < 2; j++) //Como v no es matriz de varias columnas no es necesario otro bucle for para recorrerlas, este bucle simplemente se mueve entre las columnas de A
+                {
+                    suma += A[i][j] * v[j];
+                }
+                v[i] = suma; 
+        }
+    }    
+    
+
+    for (i = 0; i<2; i++) //Producto de A*v
+    {
+        suma = 0; 
+        for (j = 0; j < 2; j++) //Como v no es matriz de varias columnas no es necesario otro bucle for para recorrerlas, este bucle simplemente se mueve entre las columnas de A
+        {
+            suma += A[i][j] * v[j];
+        }
+        Av[i] = suma; 
+    }
+    
+    Avv=0;
+    for(i=0; i<2; i++) //Producto de Av*v
+    {
+        Avv += Av[i] * v[i];
+    }
+  
+    vv=0;
+    for(i=0; i<2; i++) // Producto de v*v
+    {
+        vv += v[i] * v[i];
+    }
+
+    a=Avv/vv;
+    printf("El autovalor dominantes es %lf\n", a);
+    
+
+    return 0;
+}
