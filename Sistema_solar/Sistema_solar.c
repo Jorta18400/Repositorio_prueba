c3901a8a4ece738cb2d2f5ab5b32338b19e4d743
Jorta18400 <jforta.garcia@gmail.com>
2021-03-31 12:58:42 +0200

    Implementación de la función aceleración, aunque no funciona no se por que

--- Sistema_solar/Sistema_solar.c
+++ Sistema_solar/Sistema_solar.c
@@ -6,6 +6,7 @@
 ////////////////////////////
 
 void cambiounidades (double *rx, double *ry, double *vx, double *vy, double *m, int n);  //Función para cambiar las unidades de las condiciones iniciales 
+void aceleracion (double *rx, double *ry, double *ax, double *ay, double *m, int n); //Función para calcular las aceleraciones de cada cuerpo en un instante t
 
 
 int main(void)
@@ -19,7 +20,7 @@ int main(void)
     //Abrimos los ficheros, la estructura de estos es: #masa #posicion x #posicion y  #velocidad x #velocidad y
     fposiciones=fopen("Posiciones.txt", "w");
     fvelocidades=fopen("Velocidades.txt","w");
-    fcond=fopen("Condiciones_iniciales.txt","r+");
+    fcond=fopen("Condiciones_iniciales.txt","r");
     fenergia=fopen("Energias.txt", "w");
   
     //Definimos parámetros
@@ -53,7 +54,7 @@ int main(void)
     fprintf(fposiciones, "\n"); //Aquí introduzco un salto de línea para dejar un espacio entre cada tanda de posiciones
 
     //Lo suyo ahora sería calcular las aceleraciones para el tiempo inicial a partir de las fuerzas entre cuerpos
-
+    aceleracion(rx,ry,ax,ay,m,n);
 
 
 
@@ -77,6 +78,7 @@ int main(void)
     return 0;
 }
 
+//Funciones empleadas
 
 void cambiounidades(double *rx, double *ry, double *vx, double *vy, double *m, int n)
 {
@@ -94,6 +96,45 @@ void cambiounidades(double *rx, double *ry, double *vx, double *vy, double *m, i
         m[i]=m[i]/Ms;
         vx[i]=vx[i]*(1)/(c*sqrt(G*Ms/pow(c,3.0)));
         vy[i]=vy[i]*(1)/(c*sqrt(G*Ms/pow(c,3.0)));
+    }
+
+    return ;
+}
+
+
+void aceleracion (double *rx, double *ry, double *ax, double *ay, double *m, int n)
+{
+    int i,j; //contadores
+    double dist, axj, ayj; //dist es la distancia entre dos cuerpos y axj y ayj las componentes de aceleracion debidas a un cuerpo j concreto
+
+    i=0;
+    while(i<n) //En este while vamos a calcular la aceleración de un cuerpo i debido a la influencia de otro cuerpo j y a sumarlas para obtener la total
+    {
+        ax[i]=0.0;  //Comienzo incializando la aceleración del cuerpo a 0 por si acaso
+        ay[i]=0.0;
+
+        j=0;
+        while(j<n) //En este bucle vamos a recorrer los cuerpos j para evaluar su aporte a la aceleracion
+        {
+            if(j!=i) //Caso en que j e i son cuerpos distintos, aquí si hay aporte de aceleracion
+            {
+                dist=pow(rx[i]-rx[j],2) + pow(ry[i]-ry[j],2);
+                dist=sqrt(dist);
+
+                axj=(m[j]*(rx[i]-rx[j]))/pow(dist,3);  
+                ayj=(m[j]*(ry[i]-ry[j]))/pow(dist,3);
+
+                ax[i]=ax[i]+axj;    //Añado las aceleraciones debidas al cuerpo j al total
+                ay[i]=ay[i]+ayj;
+                
+                j++;
+            }
+            else j++; //En caso de que coincidan i y j no hay aportacion ninguna y pasamos al siguiente cuerpo
+        }
+
+        ax[i]=-ax[i];  //Cambiamos ahora de signo una vez en vez de hacer n cambios
+        ay[i]=-ay[i];
+        i++;
     }
 
     return ;
