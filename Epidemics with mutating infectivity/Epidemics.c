ea715143a6d2a1dcffa766a5f1f9caf68c9bfe26
jose <jorta@correo.ugr.es>
2022-04-19 19:06:31 +0200

    Vuelvo a empezar este programa desde otro enfoque

--- Epidemics with mutating infectivity/Epidemics.c
+++ Epidemics with mutating infectivity/Epidemics.c
@@ -14,104 +14,73 @@
 #define chi 0.001 //La probabilidad de mutación de la enfermedad
 #define lambda 0.55 //La probabilidad de infectarse que tiene un nodo si su vecino esta infectado
 #define mu 1 //La probabilidad de recuperación de un infectado
-#define tmax 1000 //Es el número de iteraciones que se realizarán
-#define Nsim 1 //Define el número de simulaciones que se van a llevar a cabo, cada simulación tiene tmax iteraciones
+#define Nsim 1000 //Define el número de simulaciones que se van a llevar a cabo, cada simulación tiene tmax iteraciones
 gsl_rng *tau; //Definimos como variable general esto para generar los números aleatorios
 
 int main(void)
 {
     extern gsl_rng *tau; 
     int i,j,k,l,simulaciones; //Contadores enteros
-    int M; //Sirve para definir el tamaño de la matriz de vecindad
-    M=N*N; //M es el número de elementos de la matriz 
-
-    int s[N+2][N+2]; //La matriz s será nuestra red o "mundo", donde cada nodo será un individuo
-    int vecindad[M][4];//En esta matriz se almacenan las conexiones entre nodos de la red(Cada fila es un nodo y las 4 columnas definen sus conexiones)(Revisar si esto se puede hacer de forma mas eficiente)
+    int M; //M es N²
+    M=N*N;
+    int x[M], A[M][M]; //El vector x contiene todos los nodos y su estado, la matriz A es la matriz de adyacencia, contiene la relación entre los nodos
     int aleatorioint; //Un número entero aleatorio
     double aleatorioreal; //Un número real aleatorio 
-    int t; //Contador de tiempo
+    int sigo; //Decide si se sigue contando el tiempo
+    double Rmedia; //Número medio de infectados por simulación
+    int I; //Esta es la cantidad de nodos infectados en la iteración dada 
     FILE *fred; //Fichero donde se guarda la red en cada iteración
+    FILE *fresultados; //Fichero donde se escriben los resultados de las simulaciones 
 
     srand(time(NULL));
-    int semilla=rand()%990001+1000; //genera una semilla aleatoria entre 10000 y 1000000 
-    tau=gsl_rng_alloc(gsl_rng_taus); //Este código nos permite después crear números aleatorios de calidad
-    gsl_rng_set(tau,semilla);
 
     fred=fopen("Red.txt", "w"); //Abro el fichero
-
-    //Inicializamos la matriz en la que, inicialmente, todos los nodos son susceptibles
-    for(i=1;i<=N;i++) //Desde 1 hasta N porque defino valores en la matriz central NxN, las condiciones periódicas rellenan la (N+2)x(N+2)
-    {
-        for(j=1;j<=N;j++)
-        {
-            s[i][j]=0; //En la matriz s, el valor 0 significará susceptible, el -1 infectado y el 1 recuperado
-        }
-    }
+    fresultados=fopen("Resultados.txt", "w");
 
     //Ahora vamos a escribir en fichero la posición inicial
-    for(j=1;j<=N;j++)
-    {
-        for(l=1;l<=N;l++)
-        {
-            if(l==N) //Si es el último elemento de la fila hacemos salto de línea
-            {
-                fprintf(fred, "%i\n", s[j][l]);
-            }else fprintf(fred, "%i,", s[j][l]);
-        }
-    }
-    fprintf(fred, "\n"); //Salto de línea para distinguir entre cada red
-    
-    //Recombinamos nuestra red, cada conexión se recombinará con probabilidad p, inicializando a la vez la matriz de vecindad
-    //Primero inicializo la red con la vecindad predeterminada de la red cuadrada
-    for(i=0;i<M;i++)
-    {
-        for(j=0;j<4;j++)
-        {
-            vecindad[i][j]=1; //Si el nodo vale 1 es que es vecino, si vale 0 no lo es, cada fila es un nodo y las 4 columnas sus 4 conexiones en el orden de              
-        }                      //arriba,derecha,abajo,izquierda   
-    }
-    k=0;
-    for(i=0;i<M;i++)   
-    {
-        k=k+1; //Un contador pa saber en que fila estamos digamos 
-        for(j=0;j<4;j++) 
-        {
-            if(vecindad[i][j]=1) //un poco cutre pero bueno, creo que funcionará, revisar si se puede hacer mejor
-            {
-                aleatorioreal=gsl_rng_uniform(tau); //Real aleatorio entre 0 y 1
-                if(aleatorioreal<=p)
-                {
-                    vecindad[i][j]=0; 
-                    if(j==0 && i>=N) vecindad[i-N][2]=0; //Todos estos if son para indicar al vecino que ya no está conectado con el nodo s(i,j)
-                    else if(j==0) vecindad[N-i][2]=0; //si i es menor que N esta en la fila de arriba y hay que conectarlo con la de abajo
-                    if(j==1 && k==N)  
-                    {
-                        k=0;
-                        vecindad[i+1-N][3]=0; //Cuando estemos en la derecha del todo y se rompa el enlace derecho tenemos que irnos a la izquierda del todo
-                    }else if(j==1) vecindad[i+1][3]=0;
-                    if(j==2 && (M-i)<=N) vecindad[M-i-1][0]=0; //Aquí al estar abajo del todo nos vamos arriba del todo 
-                    else if(j==2) vecindad[i+N][0]=0;
-                    if(j==3 && k==1) vecindad[i+N-1][1]=0; //Estando en la izquierda del todo nos lleva a la derecha del todo
-                    else if(j==3) vecindad[i-1][1]=0;
-                } 
-            }
-        }
-    }
+//    for(j=1;j<=N;j++)
+//    {
+//        for(l=1;l<=N;l++)
+//        {
+//            if(l==N) //Si es el último elemento de la fila hacemos salto de línea
+//            {
+//                fprintf(fred, "%i\n", s[j][l]);
+//            }else fprintf(fred, "%i,", s[j][l]);
+//        }
+//    }
+//    fprintf(fred, "\n"); //Salto de línea para distinguir entre cada red 
 
-    //Con la red ya inicializada y recombinada procedemos a infectar un nodo
-    k=0;
-    l=0; 
-    k=gsl_rng_uniform_int(tau,N)+1;
-    l=gsl_rng_uniform_int(tau,N)+1; //Genero dos enteros aleatorios entre 1 y N que deciden la posición del nodo infectado
-    s[k][l]=-1; 
+    Rmedia=Nsim; //Lo inicializo como Nsim porque en cada simulación se infecta un nodo aleatorio para empezar y no lo estoy contando
+    simulaciones=0;
 
     for(simulaciones=0;simulaciones<Nsim;simulaciones++) //Número de simulaciones que se llevarán a cabo
     {
-        //Ahora podemos iniciar los pasos de tiempo
-        for(t=0;t<tmax;t++)
+        //Inicializamos la matriz en la que, inicialmente, todos los nodos son susceptibles
+        for(i=0;i<M;i++)
         {
+            x[i]=0; //0 es susceptible, +1 es Removed y -1 infectado, las mutaciones tendrán distintos números negativos
+            for(j=0;j<M;j++) //Voy a incializar la matriz de adyacencia como si fuera una red cuadrada inicialmente, luego cambiaré algunas conexiones
+            {
+                
+            }
+        }
+        //Cada simulación cambiamos la semilla para generar números aleatorios distintos cada vez
+        int semilla=rand()%990001+1000; //genera una semilla aleatoria entre 10000 y 1000000 
+        tau=gsl_rng_alloc(gsl_rng_taus); //Este código nos permite después crear números aleatorios de calidad
+        gsl_rng_set(tau,semilla); 
+
+        //Con la red ya inicializada y recombinada procedemos a infectar un nodo
+        k=0;
+        k=gsl_rng_uniform_int(tau,M); //Genero dos enteros aleatorios entre 0 y M-1 que deciden la posición del nodo infectado
+        x[k]=-1;
+
+        //Ahora podemos iniciar los pasos de tiempo
+        sigo=1; //Con esta variable entera decido cuando se acaba esta simulación
+        while(sigo==1)
+        {
+            I=0; //Inicializo a 0 I en cada iteración porque es el número de infectados solo en la iteración
             //Antes de nada, establecemos las condiciones periódicas
-            for(j=0;j<=N;j++) //Hice una matriz (N+2)x(N+2) de forma que la matriz NxN es la verdadera y los puntos alrededor de esta se usan para crear las condiciones de contorno
+            for(j=0;j<=N;j++) //Comenzaré con una red 
             {
                 s[0][j]=s[N][j];   
                 s[j][0]=s[j][N];
@@ -125,55 +94,25 @@ int main(void)
                 {
                     if(s[i][j]==-1) //Si el nodo está infectado, entonces miro a sus vecinos a ver si se ponen malitos
                     {
-                        if(vecindad[N*i-N+j-1][0]==1) //Si sigue siendo vecino del predeterminado, miro a ese vecino, si no hago una tirada aleatoria a ver con quien se enlaza
+                        if(s[i+1][j]==0) //si el nodo vecino es susceptible, tiramos los dados a ver si se infecta
                         {
-                            if(s[i+1][j]==0) //si el nodo vecino es susceptible, tiramos los dados a ver si se infecta
-                            {
-                                aleatorioreal=gsl_rng_uniform(tau); //Generamos un real entre 0 y 1  
-                                if(aleatorioreal<=lambda) s[i+1][j]=2; //El valor 2 es un valor de espera, tras la iteración los nodos de valor dos se convertirán en -1, que si no luego los recién infectados infectan en esta iteración también
-                            }
-                        }else if(vecindad[N*i-N+j-1][0]==0) //Si la conexión estaba rota, le buscamos un vecino nuevo que no sea él mismo
-                        {
-                            aleatorioreal=gsl_rng_uniform(tau);
-                            if(aleatorioreal<=p) 
-                            {
-                                k=i;
-                                l=j;
-                                aleatorioreal=gsl_rng_uniform(tau);
-                                while(k==i && l==j)  //generamos una posición aleatoria que no sea el propio nodo
-                                {
-                                k=gsl_rng_uniform_int(tau,N)+1; //Genera aleatorio entre 1 y N
-                                l=gsl_rng_uniform_int(tau,N)+1;
-                                }
-                                if(s[k][l]==0)
-                                {
-                                if(aleatorioreal<=lambda) s[k][l]=2;
-                                } //No termino de ver esto ¿Como aviso al otro nodo de que esta conectado a este?s
-                            }
+                            aleatorioreal=gsl_rng_uniform(tau); //Generamos un real entre 0 y 1  
+                            if(aleatorioreal<=lambda) s[i+1][j]=2; //El valor 2 es un valor de espera, tras la iteración los nodos de valor dos se convertirán en -1, que si no luego los recién infectados infectan en esta iteración también
                         }
-                        if(vecindad[N*i-N+j-1][1]==1)
+                        if(s[i][j+1]==0)
                         {
-                            if(s[i][j+1]==0)
-                            {
-                                aleatorioreal=gsl_rng_uniform(tau);   
-                                if(aleatorioreal<=lambda) s[i][j+1]=2; 
-                            }
+                            aleatorioreal=gsl_rng_uniform(tau);   
+                            if(aleatorioreal<=lambda) s[i][j+1]=2; 
                         }
-                        if(vecindad[N*i-N+j-1][2]==1)
+                        if(s[i-1][j]==0)
                         {
-                            if(s[i-1][j]==0)
-                            {
-                                aleatorioreal=gsl_rng_uniform(tau);   
-                                if(aleatorioreal<=lambda) s[i-1][j]=2; 
-                            }
+                            aleatorioreal=gsl_rng_uniform(tau);   
+                            if(aleatorioreal<=lambda) s[i-1][j]=2; 
                         }
-                        if(vecindad[N*i-N+j-1][2]==1)
+                        if(s[i][j-1]==0)
                         {
-                            if(s[i][j-1]==0)
-                            {
-                                aleatorioreal=gsl_rng_uniform(tau);   
-                                if(aleatorioreal<=lambda) s[i][j-1]=2; 
-                            }
+                            aleatorioreal=gsl_rng_uniform(tau);   
+                            if(aleatorioreal<=lambda) s[i][j-1]=2; 
                         }
                         aleatorioreal=gsl_rng_uniform(tau);
                         if(aleatorioreal<=mu) s[i][j]=1; //El nodo infectado se recupera con probabilidad mu
@@ -191,32 +130,40 @@ int main(void)
             {
                 for(j=1;j<=N;j++)
                 {
-                    if(s[i][j]==2) s[i][j]=-1; //Volvemos a recorrer la matriz y transformamos los nodos que valen 2 en infectados
+                    if(s[i][j]==2) 
+                    {
+                    s[i][j]=-1; //Volvemos a recorrer la matriz y transformamos los nodos que valen 2 en infectados
+                    I++;
+                    }
                 }
+            }
+            Rmedia=Rmedia+I;
+            if(I==0)
+            {
+                sigo=0; //Si no hubo infectados esta iteración damos por finalizada la simulación
             }
             //Ahora vamos a escribir en fichero la posición actual
-            for(j=1;j<=N;j++)
-            {
-                for(l=1;l<=N;l++)
-                {
-                    if(l==N) //Si es el último elemento de la fila hacemos salto de línea
-                    {
-                        fprintf(fred, "%i\n", s[j][l]);
-                    }else fprintf(fred, "%i,", s[j][l]);
-                }
-            }
-            fprintf(fred, "\n"); //Salto de línea para distinguir entre cada red
+//            for(j=1;j<=N;j++)
+//            {
+//                for(l=1;l<=N;l++)
+//                {
+//                    if(l==N) //Si es el último elemento de la fila hacemos salto de línea
+//                    {
+//                        fprintf(fred, "%i\n", s[j][l]);
+//                    }else fprintf(fred, "%i,", s[j][l]);
+//                }
+//            }
+//            fprintf(fred, "\n"); //Salto de línea para distinguir entre cada red
         }
     }
+    Rmedia=Rmedia/(Nsim*1.0);
+    //Ahora vamos a escribir en el fichero cuantos removed hubo de media en cada simulación
+    fprintf(fresultados, "El número medio de infectados por simulación fue: %lf", Rmedia); 
+
+
     fclose(fred);
+    fclose(fresultados);
 
     return 0;
 }
-
-
-
-
-
-
-
 
